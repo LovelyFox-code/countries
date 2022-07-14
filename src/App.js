44c749903473197c35eb59e080d42d48e31d10f2
Alina <alinadakhno60@gmail.com>
2022-07-14 21:52:32 +0200

    create one useState

--- src/App.js
+++ src/App.js
@@ -7,18 +7,16 @@ import { iso6392 } from "iso-639-2";
 const Dropdown = styled.div`
   position: relative;
   display: inline-block;
-  width: 200px;
-  height: 220px;
   margin-top: 10%;
 `;
 const DropdownContent = styled.ul`
   min-width: 160px;
   z-index: 1;
   padding: initial;
+  display: flex;
+  align-items: center;
 `;
-const Item = styled.li`
-  list-style-type: none;
-`;
+
 const Img = styled.img`
   width: 200px;
 `;
@@ -32,97 +30,100 @@ const Flex = styled.div`
   padding: 10px 24px;
   border-radius: 18px;
 `;
-const Btn = styled.button`
+const Load = styled.img`
+  position: absolute;
+  top: 10%;
+  left: 22%;
+`;
+const Input = styled.input`
   width: 200px;
   padding: 10px 24px;
   color: green;
   font-weight: 700;
   font-size: 18px;
   border: 2px solid green;
-  border-radius: 18px;
+  border-radius: 12px;
+  outline: none;
+  margin-left: 30px;
 `;
-const Load = styled.img`
-  position: absolute;
-  top: 10%;
-  left: 22%;
+const Error = styled.h1`
+  color: red;
 `;
 function App() {
-  console.log(iso6392, "ISO");
-  const [data, setData] = useState([]);
-  const [show, setShow] = useState(false);
-  const [lang, setLang] = useState("es");
-  const langsOptions = [
-    {
-      language: "Dutch",
-      code: "nl",
-    },
-    {
-      language: "English",
-      code: "eng",
-    },
-    {
-      language: "Espanol",
-      code: "es",
-    },
-  ];
-
-  function toggle() {
-    setShow((wasOpened) => !wasOpened);
-  }
+  // const [data, setData] = useState([]);
+  // const [lang, setLang] = useState("nl");
+  // const [error, setError] = useState("");
+  const [apiStatus, setApiStatus] = useState({
+    data: [],
+    lang: "nl",
+    error: "",
+  });
   useEffect(() => {
-    setData([]);
     async function getCountryData() {
       const response = await fetch(
-        `https://deelay.me/2000/https://restcountries.com/v2/lang/${lang}`
+        `https://deelay.me/2000/https://restcountries.com/v2/lang/${apiStatus.lang}`
       );
       const langData = await response.json();
-      setData(langData);
-      // setLang(langData);
+      if (response.status === 404) {
+        setApiStatus((prevStatus) => {
+          return { ...prevStatus, data: [], error: "Not found" };
+        });
+        return;
+      }
+      if (response.status === 200) {
+        setApiStatus((prevStatus) => {
+          return { ...prevStatus, data: [langData] };
+        });
+      }
+      setApiStatus((prevStatus) => {
+        return { ...prevStatus, error: "" };
+      });
+      console.log("RESPONSE", response);
     }
     getCountryData();
-    // document.addEventListener("onClick", async () => {
-    //   try {
-    //   } catch (e) {
-    //     console.log("Error");
-    //   }
-    // });
-  }, [lang]);
-  console.log("DATA", data);
+  }, [apiStatus.lang]);
+
   return (
     <div className="App">
       <div className="main">
         <Dropdown>
-          <Btn onClick={toggle}>Language</Btn>
-          {show && (
-            <DropdownContent>
-              <label for="language">Enter what you are looking for</label>
-              <input type="text" name="language" list="languageList" />
-              <datalist id="languageList">
-                {iso6392.map((langOption) => {
-                  return (
-                    <option
-                      // value={langOption.iso6392B}
-                      onClick={() => setLang(langOption.iso6392B)}
-                    >
-                      {langOption.name}
-                    </option>
-                  );
-                })}
-              </datalist>
-            </DropdownContent>
-          )}
+          <DropdownContent>
+            <label for="language">Enter what you are looking for</label>
+            <Input
+              type="text"
+              name="language"
+              list="languageList"
+              onChange={(e) => {
+                const languageCode = iso6392.find(
+                  (langOption) => langOption.name === e.target.value
+                );
+                setApiStatus((prevStatus) => {
+                  return { ...prevStatus, lang: [languageCode.iso6392B] };
+                });
+              }}
+            />
+            <datalist id="languageList">
+              {iso6392.map((langOption) => {
+                return <option>{langOption.name}</option>;
+              })}
+            </datalist>
+          </DropdownContent>
         </Dropdown>
+        <Error>{apiStatus.error}</Error>
         <div>
-          {data.length === 0 ? (
+          {apiStatus.data.length === 0 && apiStatus.error === "" ? (
             <Load src={loadSpin} alt="loadSpin" />
           ) : (
-            data.map((el) => {
-              return (
-                <Flex key={el.id}>
-                  <p>{el.name}</p>
-                  <Img src={el.flag} alt="flag" />
-                </Flex>
-              );
+            apiStatus.data.map((el) => {
+              console.log("apiStatus.data", apiStatus.data);
+              return el.map((item) => {
+                return (
+                  <Flex key={item.id}>
+                    <p>{item.name}</p>
+                    <Img src={item.flag} alt="flag" />
+                  </Flex>
+                );
+              });
             })
           )}
         </div>
